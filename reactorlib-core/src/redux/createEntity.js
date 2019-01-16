39622e61d80e645b6da97f5523302eaf80623e48
arnel <aenero@gmail.com>
2019-01-16 23:37:21 +0800

    Added support for startup step in async reactions.

--- reactorlib-core/src/redux/createEntity.js
+++ reactorlib-core/src/redux/createEntity.js
@@ -8,32 +8,34 @@ export const createEntity = (reactions, initialState = {}) => {
     handlers[type] = handler;
   };
 
+  const registerAsyncHandler = (type, handler, count) => {
+    if (count === 3) registerHandler(`${type}Start`, handler[0]);
+    __actionCreators[type] = payload => dispatch => {
+      if (count === 3) dispatch(__actionCreators[`${type}Start`](payload));
+      handler[count - 2](payload, nextPayload =>
+        dispatch(__actionCreators[`${type}End`](nextPayload))
+      );
+    };
+    registerHandler(`${type}End`, handler[count - 1]);
+  };
+
   for (let k in reactions) {
     if (__actionCreators[k] && process.env.NODE_ENV !== 'production')
-      console.warn(
-        `Multiple entities respond to action '${k}'. Intentional?`
-      );
+      console.warn(`Multiple entities respond to action '${k}'. Intentional?`);
 
     if (typeof reactions[k] === 'function') {
       registerHandler(k, reactions[k]);
     } else if (reactions[k] instanceof Array) {
-      __actionCreators[k] = payload => dispatch => {
-        reactions[k][0](payload, nextPayload =>
-          dispatch(__actionCreators[`${k}Done`](nextPayload))
-        );
-      };
-      registerHandler(`${k}Done`, reactions[k][1]);
+      registerAsyncHandler(k, reactions[k], reactions[k].length);
     }
   }
 
-  const reducer = (state = initialState, { type, payload }) => {
+  return (state = initialState, { type, payload }) => {
     for (let k in handlers) {
       if (type === k) return handlers[k](state, payload);
     }
     return state;
   };
-
-  return reducer;
 };
 
 export default createEntity;
