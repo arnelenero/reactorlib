eb851a2bc4ec3b7589e2d218285350c2cfe028a6
Arnel Enero <aenero@gmail.com>
2019-01-31 21:04:23 +0800

    Updated http helper

--- packages/core/src/http/http.js
+++ packages/core/src/http/http.js
@@ -1,7 +1,7 @@
-export const request = async (url, options) => {
+export const request = async (url, options, onHttpError) => {
   const response = await fetch(url, options);
-  if (!response.ok) throw Error(response.statusText);
-  return await response.json();
+  if (!response.ok && typeof onHttpError === 'function') onHttpError(response);
+  return response.json();
 };
 
 export const http = (
@@ -10,7 +10,7 @@ export const http = (
   requestFn = request,
   formatFn = JSON.stringify
 ) => {
-  const verb = (method, { headers, body, ...options }) => {
+  const verb = (method, { headers, body, ...options } = {}, onHttpError) => {
     defaultHeaders = {
       'Content-Type': 'application/json',
       ...defaultHeaders,
@@ -18,14 +18,14 @@ export const http = (
     headers = { ...defaultHeaders, ...headers };
     body = formatFn(body);
     options = { ...options, body, headers };
-    return requestFn(url, { ...options, method });
+    return requestFn(url, { ...options, method }, onHttpError);
   };
 
   return {
-    get: options => verb('GET', options),
-    post: options => verb('POST', options),
-    put: options => verb('PUT', options),
-    delete: options => verb('DELETE', options),
+    get: (options, onHttpError) => verb('GET', options, onHttpError),
+    post: (options, onHttpError) => verb('POST', options, onHttpError),
+    put: (options, onHttpError) => verb('PUT', options, onHttpError),
+    delete: (options, onHttpError) => verb('DELETE', options, onHttpError),
   };
 };
 
